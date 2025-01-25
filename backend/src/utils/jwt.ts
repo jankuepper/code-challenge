9a4aa83042ea8e7b42f8063574913369aa23d062
Jan <janluca0000046@gmail.com>
2025-01-25 13:04:02 +0100

    feat: added refreshToken capability

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -1,4 +1,5 @@
-import { sign, verify } from "jsonwebtoken";
+import { JwtPayload, sign, verify } from "jsonwebtoken";
+import { isAString } from "./isString";
 
 // this is essentially treated as an all access token
 // since there is only the update functionality no claims are used
@@ -10,4 +11,12 @@ export function generateToken(username: string) {
 
 export function validateToken(token: string) {
   return verify(token, `${process.env.JWT_SECRET}`);
+}
+
+export function refreshToken(token: string) {
+  const payload = verify(token, `${process.env.JWT_SECRET}`);
+  if (isAString(payload) || !("username" in payload)) {
+    throw Error("Token is invalid.");
+  }
+  return generateToken(payload.username);
 }
